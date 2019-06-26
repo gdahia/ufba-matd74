70de0f4c56442e71742e90ed6c55fe4d17d2e060
Gabriel Dahia <gdahia@gmail.com>
2019-06-26 17:59:33 -0300

    Make run_instance.out read instance from file instead of stdin

--- implementation/src/run_instance.cpp
+++ implementation/src/run_instance.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
@@ -16,16 +17,28 @@ bool dfs(const int v, std::vector<bool> &vis,
   return true;
 }
 
-int main() {
+int main(const int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "Usage " << argv[0] << " path_to_instance" << std::endl;
+    return 1;
+  }
+
+  // open instance file
+  std::ifstream instance(argv[1]);
+  if (!instance.is_open()) {
+    std::cerr << "Unable to open instance in path " << argv[1] << std::endl;
+    return 1;
+  }
+
   // read graph main info
   int n, m, ignore;
-  std::cin >> n >> m >> ignore;
+  instance >> n >> m >> ignore;
 
   // read edges
   std::vector<std::vector<int>> adj(n);
   while (m--) {
     int u, v;
-    std::cin >> u >> v >> ignore;
+    instance >> u >> v >> ignore;
     u--, v--;
     adj[u].push_back(v);
     adj[v].push_back(u);
